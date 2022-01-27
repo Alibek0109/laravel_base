d6cac40e1b15de99d6db58746b3f5ac4560c8319
Kidiraliev Alibek <kidiraliev97@inbox.ru>
2022-01-27 11:47:57 +0500

    bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Document</title>
 </head>
+
 <body>
-    <div class="">
-        <nav>
-            <ul>
-                <li><a href="{{route('main.index')}}">Main</a></li>
-                <li><a href="{{route('post.index')}}">Posts</a></li>
-                <li><a href="{{route('about.index')}}">About</a></li>
-                <li><a href="{{route('contact.index')}}">Contact</a></li>
-            </ul>
-        </nav>
+
+
+    <div class="container">
+        <div class="row">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('main.index') }}">Main</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('post.index') }}">Posts</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('about.index') }}">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('contact.index') }}">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        @yield('content')
     </div>
 
-    @yield('content')
+
 </body>
+
 </html>
