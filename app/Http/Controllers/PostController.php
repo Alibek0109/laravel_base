391159664f7713f20742f2b53bf3d5e37abaf235
Kidiraliev Alibek <kidiraliev97@inbox.ru>
2022-01-28 10:04:16 +0500

    manyToMany

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $category = Category::find(1);
-        $post = Post::find(1);
-        dd($post->category);
+        $post = Post::find(3);
+        $tag = Tag::find(1);
+        dd($tag->posts);
 
         // return view('post.index', ['posts' => $posts]);
     }
